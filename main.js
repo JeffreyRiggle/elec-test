4f2befd73cdf7007bb4d82d8e184a7831046d548
JeffreyRiggle <JeffreyRiggle@gmail.com>
2018-01-26 06:44:05 -0500

    Introduce aurelia, webpack and electon builder

--- main.js
+++ main.js
@@ -11,10 +11,12 @@ function createWindow() {
     Menu.setApplicationMenu(null);
 
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
+        pathname: path.join(__dirname, './build/index.html'),
         protocol: 'file',
         slashes: true
     }));
+
+    win.webContents.openDevTools();
 
     win.on('closed', () => {
         win = null;
